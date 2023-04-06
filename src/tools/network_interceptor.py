b8cb6a67b8cfefac8eb09008b79e53aea7a2a0ee
nicolas.sutter <nicolas.sutter@epita.fr>
2023-04-06 12:22:51 +0200

    adding malious url detection

--- src/tools/network_interceptor.py
+++ src/tools/network_interceptor.py
@@ -1,4 +1,7 @@
 import scapy.all as scapy
+from scapy.all import ARP
+from scapy.layers.http import HTTPRequest
+import re
 
 # Create a dictionary to store the MAC-IP bindings
 mac_ip_dict = {}
@@ -7,19 +10,34 @@ mac_ip_dict = {}
 def add_mac_ip(mac, ip):
     mac_ip_dict[mac] = ip
 
-# Define a function to check for ARP poisoning
-def check_poisoning(pkt):
-    if pkt[scapy.ARP].op == 2:  # ARP response
-        if pkt[scapy.ARP].hwsrc != mac_ip_dict.get(pkt[scapy.ARP].psrc):
-            print("ARP poisoning detected! Source MAC: " + str(pkt[scapy.ARP].hwsrc) +
-                  " IP: " + str(pkt[scapy.ARP].psrc) +
-                  " Actual MAC: " + str(mac_ip_dict.get(pkt[scapy.ARP].psrc)))
+# Sniffing for http request with ip in uri
+def http_uri(pkt):
+    # Regex to analise URL
+    ip_regex = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+    
+    # get the requested URL
+    url = pkt[HTTPRequest].Host.decode() + pkt[HTTPRequest].Path.decode()
+    if (ip_regex.search(url)):
+        print("WARNING :\nHTTP access with ip in URL : http://",url)
 
+# Analising packets 
+def analisis(pkt):
+    # if this packet is an ARP Response
+    if pkt.haslayer(ARP) and pkt[scapy.ARP].op == 2:
+        if pkt[scapy.ARP].hwsrc != mac_ip_dict.get(pkt[scapy.ARP].psrc):
+            print("WARNING :\nARP poisoning detected! Source MAC: " + str(pkt[scapy.ARP].hwsrc) +
+                  "\nIP: " + str(pkt[scapy.ARP].psrc) +
+                  "\nActual MAC: " + str(mac_ip_dict.get(pkt[scapy.ARP].psrc)))
+    # if this packet is an HTTP Request
+    elif pkt.haslayer(HTTPRequest):
+        http_uri(pkt)
+    
 def sniffing_network(ip_adress):
     # Start sniffing network traffic
-    scapy.sniff(prn=check_poisoning, filter="arp", store=0)
+    scapy.sniff(prn=analisis, filter="arp or port 80", store=0)
 
     # Add MAC-IP bindings to the dictionary as they are discovered
     scapy.arping(ip_adress, verbose=0, timeout=1, retry=0, store=0, prn=add_mac_ip)
+
 
 

d4b4f623a65a35a7096679cb187586d1b03b6dde
stanfrbd <stan@thinkpad>
2023-04-07 20:44:10 +0200

    Updated tests

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker stop python-securite;
-docker rm python-securite;
-docker build -t python-securite:dev .;
-docker run -v "$PWD/:/opt/python-securite" --name python-securite -it python-securite:dev;
+docker-compose -f "$PWD/tests/docker-compose.yml" down ;
+docker-compose -f "$PWD/tests/docker-compose.yml" up -d ;
+docker stop python-securite ;
+docker rm python-securite ;
+docker build -t python-securite:dev . ;
+docker run -v "$PWD/:/opt/python-securite" --name python-securite -it --ip 172.18.1.10 --network test_network python-securite:dev ;

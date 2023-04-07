6bc616d60c80f66155a0f74c0b40760e7cb2f37e
stanfrbd <stan@thinkpad>
2023-04-07 21:24:49 +0200

    Updated tests

--- tests/non_interactive_run.sh
+++ tests/non_interactive_run.sh
@@ -5,4 +5,4 @@ docker-compose -f "$PWD/tests/docker-compose.yml" up -d ;
 docker stop python-securite ;
 docker rm python-securite ;
 docker build -t python-securite:dev . ;
-docker run -it -v "$PWD/:/opt/python-securite" --name python-securite --ip 172.18.1.10 --network test_network python-securite:dev /bin/bash /opt/python-securite/tests/run_tests.sh ;
+docker run -v "$PWD/:/opt/python-securite" --name python-securite --ip 172.18.1.10 --network test_network python-securite:dev /bin/bash /opt/python-securite/tests/run_tests.sh ;

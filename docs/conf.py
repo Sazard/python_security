42c5264cbc742f1a73965d331884c89ebf6b7206
stanfrbd <stan@thinkpad>
2023-04-12 22:38:32 +0200

    Updated doc

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../src'))
@@ -31,5 +32,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
